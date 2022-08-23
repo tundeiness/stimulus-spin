36b7bd5dea2864dd902e2c02949997103c85d898
tundeiness <tundeiness@yahoo.co.uk>
2022-08-23 17:12:25 +0100

    complete initial iteration of slide show

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -2,6 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slideshow"
 export default class extends Controller {
+
+  static targets = ['slide']
+
+  initialize(){
+    this.index = 0
+    this.showCurrentSlide();
+  }
+
+  next(){
+    this.index++
+    this.showCurrentSlide();
+  }
+
+  previous(){
+    this.index--
+    this.showCurrentSlide();
+  }
+
+
   connect() {
+  }
+
+  showCurrentSlide(){
+    this.slideTargets.forEach((ele, index)=>{
+      ele.hidden = index != this.index
+    })
   }
 }
