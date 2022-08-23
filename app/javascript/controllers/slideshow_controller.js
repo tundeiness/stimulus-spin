cd16ae2499d30ee5476a48bf4d54e102d5823ecb
tundeiness <tundeiness@yahoo.co.uk>
2022-08-23 17:36:44 +0100

    refactor code

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -4,29 +4,54 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = ['slide']
+  //  defining values by describing them in a static object property called values
+  //  in this case the single numeric value is called index
+  static values = {index: Number}
 
-  initialize(){
-    this.index = 0
-    this.showCurrentSlide();
-  }
+  // setting default values
+  // this will start the index at 2 if no data-slideshow-index-value attribute was defined
+  // on the controller element
+  // static values = { index: { type: Number, default:2 } }
 
-  next(){
-    this.index++
-    this.showCurrentSlide();
-  }
+  //  for other kinds of value state the following
+  // static values = { index: Number, effect: { type: String, default: "kenburns" } }
 
-  previous(){
-    this.index--
-    this.showCurrentSlide();
-  }
+  // initialize(){
+    // set this.index for controller to track state
+    // this.index = 0
+
+    // approach to start the slide show with the second slide
+    //  read the attribute declared as data-index='1' in index.html
+    // this.index = Number(this.element.dataset.index)
+    // this.showCurrentSlide();
+  // }
+
 
 
   connect() {
   }
 
+  next(){
+    // this.index++
+    this.indexValue++;
+    // this.showCurrentSlide();
+  }
+
+  previous(){
+    // this.index--
+    this.indexValue--;
+    // this.showCurrentSlide();
+  }
+
+
   showCurrentSlide(){
     this.slideTargets.forEach((ele, index)=>{
-      ele.hidden = index != this.index
+      // ele.hidden = index != this.index
+      ele.hidden = index != this.indexValue
     })
+  }
+
+  indexValueChanged() {
+    this.showCurrentSlide()
   }
 }
