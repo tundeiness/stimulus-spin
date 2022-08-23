2d93ef2e6d0b9444e2ac0ea38ee11b710d290b1a
tundeiness <tundeiness@yahoo.co.uk>
2022-08-23 14:28:13 +0100

    complete using stimulus to copy text to clipboard

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,6 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
+  //  define a static array called targets 
+  static targets = ['origin']
+  // the above makes the following available to us: this.originTarget
+  // this.originTargets evaluate to an array of all origin targets in the controller's scope.
+  // this.hasOriginTarget evaluates to true if there is a origin target or false if not.
+
+
   connect() {
+  }
+
+  copy(){
+    // select input field's content and call the clipboard API
+    navigator.clipboard.writeText(this.originTarget.value)
+  }
+
+
+  copy(event){
+    event.preventDefault()
+    // select input field's content and call the clipboard API
+    navigator.clipboard.writeText(this.originTarget.value)
   }
 }
