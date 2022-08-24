603b47ed420dcbd866e6808aeff5db1c33bf3255
tundeiness <tundeiness@yahoo.co.uk>
2022-08-24 12:56:55 +0100

    create loader controller

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -2,6 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loader"
 export default class extends Controller {
+ 
+
+  static values = {url: String}
+
   connect() {
+    this.load()
+  }
+
+  load() {
+    fetch(this.urlValue)
+    .then(res => res.text())
+    .then(html => this.element.innerHTML = html)
   }
 }
